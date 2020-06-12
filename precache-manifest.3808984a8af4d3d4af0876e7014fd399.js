3d4104346bf7335638ed584c2fe3a3b6a4d2d965
Robin Dhull <rsdhull707@gmail.com>
2020-06-12 10:05:58 +0530

    Updates

---
+++ precache-manifest.3808984a8af4d3d4af0876e7014fd399.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece2511dfa874cb10b8fb96b370a4a89",
+    "url": "/React-Avatar-Project/index.html"
+  },
+  {
+    "revision": "5923ef7502620500ad6a",
+    "url": "/React-Avatar-Project/static/css/2.d811125f.chunk.css"
+  },
+  {
+    "revision": "44081d774ed645ca887d",
+    "url": "/React-Avatar-Project/static/css/main.09bbea63.chunk.css"
+  },
+  {
+    "revision": "5923ef7502620500ad6a",
+    "url": "/React-Avatar-Project/static/js/2.2fa55961.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Avatar-Project/static/js/2.2fa55961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44081d774ed645ca887d",
+    "url": "/React-Avatar-Project/static/js/main.97d5d6cc.chunk.js"
+  },
+  {
+    "revision": "03bcadf08f8c528016a9",
+    "url": "/React-Avatar-Project/static/js/runtime-main.b3faf02d.js"
+  }
+]);
